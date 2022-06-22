c78eb9fa654709f8131a13bdf2179fa505f5cced
Перунова Анастасия <aperunova@atsaero.ru>
2022-06-22 20:12:47 +0200

    Solve CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,10 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      cnt: 0,
+    };
+  },
+}).mount('#app');
