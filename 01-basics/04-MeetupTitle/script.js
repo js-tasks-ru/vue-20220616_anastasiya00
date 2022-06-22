bbafee614797e82207b0eb4e2e2af9ae32d90443
Перунова Анастасия <aperunova@atsaero.ru>
2022-06-22 20:49:15 +0200

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,26 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      id: null,
+      meetup: null,
+    };
+  },
+
+  watch: {
+    async id() {
+      this.meetup = await fetchMeetupById(this.id);
+    },
+  },
+
+  computed: {
+    cnt() {
+      return 5;
+    },
+    meetupTitle() {
+      return this.meetup?.title;
+    },
+  },
+}).mount('#app');
