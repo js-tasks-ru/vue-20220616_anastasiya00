4fded2581880ff3dad161f44f1e510bd79926112
Перунова Анастасия <aperunova@atsaero.ru>
2022-06-25 16:57:04 +0200

    Solve MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -4,7 +4,12 @@ export default defineComponent({
   name: 'MeetupCover',
 
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="image && { '--bg-url': \`url(\${image})\` }">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
+
+  props: {
+    title: String,
+    image: String,
+  },
 });
